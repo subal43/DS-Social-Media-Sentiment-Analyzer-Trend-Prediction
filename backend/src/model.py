8ccc0145df5e1c65dbd1e135145108e9424c51c9
subal43 <subalkundu3@gmail.com>
2025-12-16 21:09:36 +0530

    add dynamic path creation for model save and load

--- backend/src/model.py
+++ backend/src/model.py
@@ -1,6 +1,14 @@
 import pandas as pd
 import pickle
+import os
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import LogisticRegression
 from sklearn.pipeline import Pipeline
+
+MODEL_FILE = os.path.join(os.path.dirname(os.path.dirname(__file__)), '..' , 'models')
+if not os.path.exists(MODEL_FILE):
+    os.makedirs(MODEL_FILE)
+
+MODEL_PATH = os.path.join(MODEL_FILE, 'sentiment_model.pkl')
+
