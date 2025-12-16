61cef0ce9ec36192a168ebc7c4a9ec5368137912
subal43 <subalkundu3@gmail.com>
2025-12-16 20:53:26 +0530

    create model file and import necessary module

---
+++ backend/src/model.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import pickle
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import LogisticRegression
+from sklearn.pipeline import Pipeline
