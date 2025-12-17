0cce8291e3992d3b5d186f4d36c414ae4eccf3b4
subal43 <subalkundu3@gmail.com>
2025-12-17 18:56:26 +0530

    create get_data function

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,7 @@ import os
 
 sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
 from src.preprocessing import load_data, prepare_data, add_dates
-from src.model import train_model, save_model, load_model, predict_sentiment
+# from src.model import train_model, save_model, load_model, predict_sentiment
 
 st.set_page_config(
     page_title="Social Sentiment Analyzer",
@@ -14,3 +14,13 @@ st.set_page_config(
     layout="wide"
 )
 
+@st.cache_data
+def get_data():
+    if os.path.exists('data/twitter_training.csv'):
+        data_path = 'data/twitter_training.csv'
+    
+    df = load_data(data_path)
+    if not df is None:
+        df = prepare_data(df)
+        df = add_dates(df)
+    return df
