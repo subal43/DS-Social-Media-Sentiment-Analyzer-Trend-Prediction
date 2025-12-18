0633b167eb7fd57cf20c3c872ffbd25d28f0a486
subal43 <subalkundu3@gmail.com>
2025-12-18 22:10:11 +0530

    create get_model function

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,7 @@ import os
 
 sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
 from src.preprocessing import load_data, prepare_data, add_dates
-# from src.model import train_model, save_model, load_model, predict_sentiment
+from src.model import train_model, save_model, load_model, predict_sentiment
 
 st.set_page_config(
     page_title="Social Sentiment Analyzer",
@@ -24,3 +24,14 @@ def get_data():
         df = prepare_data(df)
         df = add_dates(df)
     return df
+
+@st.cache_resource
+def get_model(df=None):
+    model = load_model('models/sentiment_model.pkl')
+    if model is None and df is not None :
+        sample_size = min(len(df),5000)
+        model = train_model(df.sample(sample_size, random_state=42))
+        save_model(model, 'models/sentiment_model.pkl')
+        return model
+    else:
+        return model
